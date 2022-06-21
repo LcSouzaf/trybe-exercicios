812533a52ca9fada317550470ff2c334eeb0a4b0
Lucas Souza <lucscsf@gmail.com>
2022-06-21 18:42:57 -0300

    exercicio 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,8 @@
+let a = 8;
+let b = 5;
+console.log(soma = a +b);
+console.log(subtracao = a - b);
+console.log(multiplicacao = a * b);
+console.log(divisao= a / b);
+console.log( modulo = a % b);
+
