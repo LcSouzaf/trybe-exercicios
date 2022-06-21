d47ca5337332b2d5fe3677dff0a7f9254d6e7f35
Lucas Souza <lucscsf@gmail.com>
2022-06-21 18:59:14 -0300

    exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -6,9 +6,24 @@ console.log(multiplicacao = a * b);
 console.log(divisao = a / b);
 console.log(modulo = a % b);
 
+// exercicio 2
 if (a > b) {
     console.log(a)
 }
 else if (a < b) {
     console.log(b)
+};
+
+//exercicio 3
+
+const primeiroNumero = 44;
+const segundoNumero = 6;
+const terceiroNumero = 21;
+
+if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero) {
+    console.log(primeiroNumero)
+} else if (segundoNumero > primeiroNumero && segundoNumero > terceiroNumero) {
+    console.log(segundoNumero)
+} else if (terceiroNumero > segundoNumero && terceiroNumero> primeiroNumero) {
+    console.log(terceiroNumero)
 };
