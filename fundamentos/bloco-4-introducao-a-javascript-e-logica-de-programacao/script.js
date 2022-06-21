4d764f49e649233462bd7a4b1293935f788abff2
Lucas Souza <lucscsf@gmail.com>
2022-06-21 18:47:45 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -1,8 +1,14 @@
 let a = 8;
 let b = 5;
-console.log(soma = a +b);
+console.log(soma = a + b);
 console.log(subtracao = a - b);
 console.log(multiplicacao = a * b);
-console.log(divisao= a / b);
-console.log( modulo = a % b);
+console.log(divisao = a / b);
+console.log(modulo = a % b);
 
+if (a > b) {
+    console.log(a)
+}
+else if (a < b) {
+    console.log(b)
+};
