57df64b01fee46dd0793de4edc30d95125ea7d4c
Lucas Souza <lucscsf@gmail.com>
2022-06-22 19:00:05 -0300

    add exercicio 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02-arrays-loop-for/script.js
@@ -0,0 +1,17 @@
+// exercicio 1
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index++) {
+    console.log (numbers[index]);
+    
+} */
+
+//exercicio 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0
+for (let index = 0; index < numbers.length; index++ ) {
+    sum += numbers[index]
+}
+console.log(sum)
