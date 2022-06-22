7dd889940b942847b5640e14c35e03273f344c34
Lucas Souza <lucscsf@gmail.com>
2022-06-22 18:36:29 -0300

    altera local exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,105 @@
+/* let a = 8;
+let b = 5;
+console.log(soma = a + b);
+console.log(subtracao = a - b);
+console.log(multiplicacao = a * b);
+console.log(divisao = a / b);
+console.log(modulo = a % b);
+
+// exercicio 2
+if (a > b) {
+    console.log(a)
+}
+else if (a < b) {
+    console.log(b)
+};
+
+//exercicio 3
+
+const primeiroNumero = 44;
+const segundoNumero = 6;
+const terceiroNumero = 21;
+
+if (primeiroNumero > segundoNumero && primeiroNumero > terceiroNumero) {
+    console.log(primeiroNumero)
+} else if (segundoNumero > primeiroNumero && segundoNumero > terceiroNumero) {
+    console.log(segundoNumero)
+} else if (terceiroNumero > segundoNumero && terceiroNumero> primeiroNumero) {
+    console.log(terceiroNumero)
+};
+
+//exercicio 4
+
+const num1 = -55;
+if(num1 > 0){
+    console.log('positive')
+} else if(num1<0) {
+    console.log('negative')
+}else if (num1 == 0){
+    console.log('0')
+};
+
+//exercicio 5
+
+const angulo1 = 20;
+const angulo2 = 80;
+const angulo3 = 55;
+
+var somaAngulos = angulo1+angulo2+angulo3
+
+if(somaAngulos === 180 ){
+console.log(true)
+}else if (somaAngulos < 180){
+    console.log (false)
+}else if(somaAngulos <= 0){
+    console.log('error')
+}
+
+//exercicio 6
+
+const cavalo = 
+
+//exercicio 7
+
+let notas = 101;
+
+if (notas < 0 || notas > 100) {
+    console.log('Error')
+}else if (notas >=90 && notas <= 100) {
+    console.log('A')
+}else if (notas < 90 && notas >= 80) {
+    console.log('B')
+}else if (notas < 80 && notas >= 70) {
+    console.log('C')
+}else if (notas < 70 && notas >= 60) {
+    console.log('D')
+}else if (notas < 60 && notas >= 50) {
+    console.log('E')
+}else if (notas < 50) {
+    console.log('F')
+};
+
+//exercicio 8
+
+
+
+const numero1 = 55;
+const numero2 = 25;
+const numero3 = 22;
+
+let temPar = false;
+if(numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0){
+    temPar = (true)
+};
+console.log(temPar)  */
+
+const a = 44
+const b = 66
+const c = 22
+
+let temImpar = false
+
+if(a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0){
+    temImpar=(true)
+}
+console.log(temImpar)
