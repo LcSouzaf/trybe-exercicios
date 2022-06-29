4681e239721290c49225760116b1bd4085bfe1fb
Lucas Souza <lucscsf@gmail.com>
2022-06-29 20:29:39 -0300

    exercicios/5.2

---
+++ fundamentos/bloco-05-DOM-seletores/dia-02-trabalhando-elementos/script.js
@@ -0,0 +1,26 @@
+/* document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "blue";
+
+document.getElementById("primeiroFilhoDoFilho").innerText = "Lucas Souza";
+document.getElementById("pai").firstChild;
+
+document.getElementById("elementoOndeVoceEsta").previousElementSibling;
+
+document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+
+document.getElementById("pai").lastElementChild.previousElementSibling;
+
+let brotherVoceEsta = document.querySelector(".elementoOndeVoceEsta");
+for (let index = 0; index < brotherVoceEsta.length; index++) {
+  let brotherVoceEstaSection = brotherVoceEsta[index];
+  let brotherVoceEstaCreate = document.createElement("section");
+  brotherVoceEsta.appendChild.brotherVoceEstaCreate;
+} */
+
+/* let sonVoceEsta = document.getElementById('elementoOndeVoceEsta');
+  let sonVoceEstaCreate = document.createElement('div');
+  sonVoceEsta.id ='filhoDoFilho'
+  sonVoceEsta.appendChild(sonVoceEstaCreate);
+ */
+
+
+  
