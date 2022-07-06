2fd9213295de9dc506b7839f4cffcc50fedddf8a
Lucas Souza <lucscsf@gmail.com>
2022-07-06 16:26:54 -0300

    ARQUIVOS

---
+++ fundamentos/bloco-05/exercicios5.2/script.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").style.color = "green"
+
+document.getElementById("second-paragraph").innerText ='Lucas Souza'
+
+document.getElementsByClassName('.paragraph').innerText ='Lucas Souza'
